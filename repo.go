aa8f85de0cb26c36a5be4aa3554ae6baa16e8b69
Douglas Holanda <douglasholanda3195@gmail.com>
2024-02-10 07:59:02 -0300

    New scope functions

--- repo.go
+++ repo.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strings"
+	"container/list"
 )
 
 type Repository struct{
@@ -12,29 +13,47 @@ type Repository struct{
 }
 
 func main(){
+    listRepo := list.New()
     fmt.Println("Welcome to our Amazon Web Services repository management system.\n")
-    interactes()
+    interactes(listRepo)
 }
 
-func interactes(){
-    fmt.Println("Choose an option:\n")
+func interactes(listRepo *list.List){
+    fmt.Println("   Choose an option:   \n")
     fmt.Println("   C/c - Create repository\n" +
+                "   S/s - Search repository\n" +                
                 "   U/u - Update repository\n" +
-                "   S/s - Search repository\n"
                 "   D/d - Delete repository\n")
     var option string
     fmt.Scanln(&option)
     
     switch strings.ToLower(option){
         case "c" :
-        
-        case "u" :
-        
+            createRepo(listRepo)
         case "s" :
-        
+            searchRepository(listRepo)
+        case "u" :
+            updateRepository(listRepo)
         case "d" :
-        
+            deleteRepository(listRepo)
         default:
             fmt.Println("Option no-existent.\n")
     }
 }
+
+func createRepo(listRepo *list.List){
+    
+}
+
+func searchRepository(listRepo *list.List){
+    
+}
+
+func updateRepository(listRepo *list.List){
+    
+}
+
+func deleteRepository(listRepo *list.List){
+    
+}
+
