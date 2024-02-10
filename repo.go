909607cc4022a5ac34c67e23b17a7f5f331de87a
Douglas Holanda <douglasholanda3195@gmail.com>
2024-02-09 21:33:01 -0300

    Adding options user

--- repo.go
+++ repo.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 type Repository struct{
@@ -10,7 +11,30 @@ type Repository struct{
 	sizeRepo float64
 }
 
+func main(){
+    fmt.Println("Welcome to our Amazon Web Services repository management system.\n")
+    interactes()
+}
+
 func interactes(){
-	fmt.println("		Welcome to our Amazon Web Services repository management system	\n");
-	var option = int; 
+    fmt.Println("Choose an option:\n")
+    fmt.Println("   C/c - Create repository\n" +
+                "   U/u - Update repository\n" +
+                "   S/s - Search repository\n"
+                "   D/d - Delete repository\n")
+    var option string
+    fmt.Scanln(&option)
+    
+    switch strings.ToLower(option){
+        case "c" :
+        
+        case "u" :
+        
+        case "s" :
+        
+        case "d" :
+        
+        default:
+            fmt.Println("Option no-existent.\n")
+    }
 }
