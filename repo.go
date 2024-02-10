a317a648eac806888ba6d323360e2dbf53b8c514
Douglas Holanda <douglasholanda3195@gmail.com>
2024-02-09 21:31:48 -0300

    My first commit in Golang program

---
+++ repo.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Repository struct{
+	idRepo int
+	nameRepo string
+	sizeRepo float64
+}
+
+func interactes(){
+	fmt.println("		Welcome to our Amazon Web Services repository management system	\n");
+	var option = int; 
+}
